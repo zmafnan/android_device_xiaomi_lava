a2e6537170660917c690a92c6988ecbeeb0f9de4
zmafnan <iamzafn22@gmail.com>
2022-07-17 19:39:05 +0700

    lava: lava: Add Raven July 2022 fingerprint
    Signed-off-by: zmafnan <iamzafn22@gmail.com>

--- aosp_lava.mk
+++ aosp_lava.mk
@@ -46,3 +46,14 @@ PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := lancelot
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys" \
+    PRODUCT_NAME="lava" \
+    TARGET_DEVICE="lava"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
