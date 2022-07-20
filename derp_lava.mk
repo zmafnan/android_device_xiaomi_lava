c882b16e89ebf1112ec12eb5fbd39535242344ba
zmafnan <iamzafn22@gmail.com>
2022-07-20 15:19:51 +0700

    lava: Change Fingerprint Format
    Signed-off-by: zmafnan <iamzafn22@gmail.com>

--- derp_lava.mk
+++ derp_lava.mk
@@ -55,5 +55,12 @@ TARGET_USES_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220705.003/8671607:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ3A.220705.003 8671607 release-keys" \
+    PRODUCT_NAME="lava" \
+    TARGET_DEVICE="lava"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
